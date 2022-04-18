ccc80f2332a9c7967e87417b273e0f67c0ab8741
Evan Czyzycki <eczy3826@gmail.com>
2022-04-18 13:23:31 -0700

    remove more backups for jenkins and overleaf

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,8 +7,6 @@ rsync -a /data/redmine/files /backup/redmine
 
 cd /backup
 tar -czvf /backup/archives/redmine_`date +%Y-%m-%d`.tar.gz redmine
-tar -czvf /backup/archives/jenkins_`date +%Y-%m-%d`.tar.gz jenkins
-tar -czvf /backup/archives/sharelatex_`date +%Y-%m-%d`.tar.gz sharelatex
 
 # Delete backups older than 7 days
 find $(pwd) -mtime +7 -type f -delete
