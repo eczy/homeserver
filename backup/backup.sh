add5929ce5dd8d6877a2ae53d0e9d7ffd512d256
Evan Czyzycki <eczy3826@gmail.com>
2022-07-29 17:03:21 -0700

    Replace NGINX with Traefik

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
+# Gitea
 docker exec -u git -it -w /tmp $(docker ps -qf 'name=^gitea$') bash -c '/app/gitea/gitea dump -c /data/gitea/conf/app.ini'
 tar -czvf /backup/archives/gitea_`date +%Y-%m-%d`.tar.gz /tmp
+
+# Redmine
+PGPASSWORD=${REDMINE_DB_PASSWORD} /usr/bin/pg_dump -U ${REDMINE_DB_USERNAME} -h 'redmine-db' -Fc --file=/backup/redmine/redmine.sqlc redmine
+rsync -a /data/redmine/files /backup/redmine
+tar -C /backup/redmine -czvf /backup/archives/redmine_`date +%Y-%m-%d`.tar.gz .
 
 # Delete backups older than 7 days
 find $(pwd) -mtime +7 -type f -delete
